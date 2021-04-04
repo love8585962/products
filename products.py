92fabfd290a93c0b57f4ab74514726ad5f8aa704
Louis Chen <love8585962@gmail.com>
2021-04-04 17:13:43 +0800

    debug

--- products.py
+++ products.py
@@ -1,8 +1,8 @@
-products = []
-while True :
-    name = input("請輸入商品名稱: ")
-    if name == "q" : #逃出迴圈
-        break
-    price = input("請輸入商品價格: ")
-    products.append(name, price)
-print(products)
+products = []  #建立一個名為products的空清單
+while True :  #建立無窮迴圈
+    name = input("請輸入商品名稱: ")  #讓用戶輸入名稱
+    if name == "q" : #輸入q結束迴圈
+        break  #停止
+    price = input("請輸入商品價格: ") #讓用戶輸入價格
+    products.append([name, price]) #新增兩個名為"name","price"的兩個小清單到大清單中
+print(products) #印出結果
