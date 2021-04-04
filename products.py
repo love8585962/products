6b0d2be629dd831c94818b545b1577a43237f77a
Louis Chen <love8585962@gmail.com>
2021-04-04 17:20:48 +0800

    New

--- products.py
+++ products.py
@@ -5,4 +5,6 @@ while True :  #建立無窮迴圈
         break  #停止
     price = input("請輸入商品價格: ") #讓用戶輸入價格
     products.append([name, price]) #新增兩個名為"name","price"的兩個小清單到大清單中
-print(products) #印出結果
+
+for p in products:
+    print(p[0], '的價格是',p[1]) #印出結果
